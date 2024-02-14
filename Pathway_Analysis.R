0584d349464838b3625fa418e3268d8b856b0f2b
Karthik Raj <51468149+KrajShuffle@users.noreply.github.com>
2024-02-14 01:00:02 -0800

    Corrected Gene Names & RNA Seq Data Normalization

--- Pathway_Analysis.R
+++ Pathway_Analysis.R
@@ -8,53 +8,48 @@ opts_chunk$set(
   message = FALSE
 )
 
-##--------- Loading in Dataset & Setting index as Ensembl Gene IDs
+##--------- Loading in Dataset & Setting index as Ensembl Gene IDs-----------
 per_reps_counts = read.table('final_cleaned_all_reps.txt', sep = '\t', header = TRUE)
 rep_names = c('par_rep1','par_rep2', 'per_rep1', 'per_rep2', 'per_rep3')
 treat_factors = factor(c('control', 'control', 'treated', 'treated', 'treated'))
 col_data = data.frame(row.names = rep_names, Treatment = treat_factors)
-gene_expr = per_reps_counts[ , rep_names] # Selected all columns except gene_id column
+gene_expr_counts = per_reps_counts[ , rep_names] # Selected all columns except gene_id column
 per_reps_counts = per_reps_counts[rowSums(gene_expr) >= 1, ] # Removed empty genes
 
-##---- String Function to Remove Version IDs from Ensembl Gene IDs 
+##---- String Function to Remove Version IDs from Ensembl Gene IDs --------
 clean_vers = function(my.string) {
   unlist(strsplit(my.string, split = "\\."))[1]
 }
-per_reps_counts$Gene_ID = sapply(per_reps_counts$Gene_ID,clean_vers)
-##-------- RNA-seq Data Processing Workflow
-##--- Converting Ensembl IDs to HGNC symbols
+per_reps_counts$Gene_ID = sapply(per_reps_counts$Gene_ID,clean_vers)  # Stripped off gene version ID from Ensembl IDs
+
+##----- Converting Ensembl IDs to HGNC symbols-------------
 library(biomaRt)
+library(magrittr)
+library(dpylr)
 mart = useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
 genes = getBM(attributes = c("ensembl_gene_id","hgnc_symbol"),
               values=per_reps_counts$Gene_ID, mart=mart)
 
 matched = match(per_reps_counts$Gene_ID, genes$ensembl_gene_id)
 per_reps_counts$HGNC = genes$hgnc_symbol[matched]
+per_reps_counts$HGNC[per_reps_counts$HGNC == ""] <- NA # Replacing all empty strings as NA, since equivalent now
+
+##---- Consolidating Genes with multiple HGNC symbols--------------
 duplicated_genes = unique(per_reps_counts$HGNC[duplicated(per_reps_counts$HGNC)])
+duplicated_genes
+per_reps_empty = per_reps_counts[is.na(per_reps_counts$HGNC), ] # Genes with no HGNC symbol
+per_reps_HGNC = per_reps_counts[is.na(per_reps_counts$HGNC) == FALSE, ] # Genes with HGNC symbol
+# Combining read counts of genes with multiple Ensembl IDs
+per_reps_uniqHGNC <- aggregate(. ~ HGNC, data = per_reps_HGNC[ , colnames(per_reps_HGNC)[2:7]], FUN = sum)
+# Putting the dataframe with genes with no HGNC symbol and dataframe with genes with unique HGNC symbol together
+cleaned_reps_counts <- rbind(per_reps_uniqHGNC, per_reps_empty[ , c("HGNC","par_rep1", "par_rep2", 
+                                                                    "per_rep1", "per_rep2", "per_rep3")])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+##---- RNA-seq Data Normalization----------------
 library(DESeq2)
 # import data to DESeq2 and setup for data normalization
-dset = DESeqDataSetFromMatrix(per_reps_counts,
+dset = DESeqDataSetFromMatrix(cleaned_reps_counts[ , rep_names],
                               colData=col_data, design=~Treatment)
-dset_filter_step1 = dset[rowSums(counts(dset)) >= 1, ] # Filter out genes with 0 reads across all replicates
-dset_filtered_counts = counts(dset_filter_step1)
-dset_filter_step1 = DESeq(dset_filter_step1) # Normalization
-gene_expr = getVarianceStabilizedData(dset_filter_step1) # Applied Variance Stabilizing Transformation
-rownames(gene_expr) = sapply(rownames(gene_expr),clean_vers) # Stripped off gene version ID for Normalized Data
-rownames(dset_filtered_counts) = sapply(rownames(dset_filtered_counts), clean_vers) # Stripped off gene version ID for Count Data
-dset_filtered_counts = as.data.frame(dset_filtered_counts)
-
+dset = DESeq(dset) # Normalization
+gene_expr_VST = getVarianceStabilizedData(dset) # Applied Variance Stabilizing Transformation
 
